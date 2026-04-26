1d8e88cf42e15efdb2eeef10c905e87c45d6f395
DanilKabanov <danilkabanov2006@bk.ru>
2026-04-26 13:35:05 +0200

    Update download.py

--- download.py
+++ download.py
@@ -4,21 +4,23 @@ from sklearn.preprocessing import OrdinalEncoder
 def download_data():
     url = "https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/insurance.csv"
     df = pd.read_csv(url)
-    df.to_csv("insurance_raw.csv", index=False)
+    df.to_csv("insurance.csv", index=False)
     return df
 
 def clear_data(path2df):
     df = pd.read_csv(path2df)
-    df = df.dropna()
+    df = df.drop_duplicates().reset_index(drop=True)
+    
+    if df.isnull().sum().sum() > 0:
+        df = df.dropna()
+    
     cat_columns = ['sex', 'smoker', 'region']
     ordinal = OrdinalEncoder()
-    ordinal.fit(df[cat_columns])
-    df[cat_columns] = ordinal.transform(df[cat_columns])
-    df = df[df['bmi'] < 60]
-    df = df[df['charges'] < 60000]
+    df[cat_columns] = ordinal.fit_transform(df[cat_columns])
+    
     df.to_csv('df_clear.csv', index=False)
     return True
 
 if __name__ == "__main__":
     download_data()
-    clear_data("insurance_raw.csv")
+    clear_data("insurance.csv")
